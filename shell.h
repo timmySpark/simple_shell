90530e604ca1d667e6abf6a22a17d713fb9362ba
timmySpark <timmyspark1@gmail.com>
2023-09-12 18:35:44 -0700

    [feat] :- added prototypes to the header file

--- shell.h
+++ shell.h
@@ -1,6 +1,20 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
+/* Shell Prototypes */
+
+void interactive(void);
+void non_interactive(void);
+
+/* Core Shell Functionality */
+char *read_line(void);
+char **split_line(char *line);
+void execute_args(char **args);
 
 #endif
