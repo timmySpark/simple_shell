70f4b13c8988ca686fc60de02aacce120881cfcf
timmySpark <timmyspark1@gmail.com>
2023-09-12 18:37:20 -0700

    [feat] :- my custom shell i the making

---
+++ shellie.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * main -  check if file is connected to terminal
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	if (isatty(STDIN_FILENO) == 1)
+		interactive();
+	else
+		non_interactive();
+}
+
+
+/**
+ * interactive - functions used when shell is waiting for user input
+ *
+ */
+
+void interactive(void)
+{
+	char *line;
+	char **args;
+
+	while (1)
+	{
+		printf("#shell:) ");
+
+		line = read_line();
+
+		if (!line)
+		{
+			free(line);
+			continue;
+		}
+
+		args = split_line(line);
+		execute_args(args);
+
+		free(line);
+		free(args);
+	}
+}
+
+/**
+ * non_interactive - functions used when shell is processing automated commands
+ *
+ */
+
+void non_interactive(void)
+{
+	char *line;
+	char **args;
+
+	while (!feof(stdin))
+	{
+		line = read_line();
+
+		if (!line)
+		{
+			free(line);
+			continue;
+		}
+
+		args = split_line(line);
+		execute_args(args);
+
+		free(line);
+		free(args);
+	}
+}
