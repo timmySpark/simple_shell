e4c7822cea95b8c64d784af5e9d1c895a81a0c1c
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-09-14 13:05:57 -0700

    more_doings

--- second_functionalities.c
+++ second_functionalities.c
@@ -21,3 +21,46 @@ int execute_args(char **args, **envp)
 		if (execve(command_path, args, envp) == -1)
 		{
 			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+	else if (pid > 0)
+	{
+		waitpid(pid, &status, 0);
+
+		if (WIFEXITED(status)) 
+		{
+			return (WEXITSTATUS(status));
+		}
+		else 
+		{
+			perror("fork");
+			return (-1);
+		}
+	}
+
+char *find_command(const char *command, char *envp)
+{
+	char *path = getenv("PATH");
+	char *token, *saveptr;
+
+	token = strtok_r(path, ":", &saveptr);
+
+	while (token != NULL)
+	{
+		char *full_path = malloc(strlen(token) + strlen(command) + 2);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, command);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			return (full_path);
+		}
