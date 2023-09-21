e7f362450a50a48e07f6585c927e3728481f7e4f
timmySpark <timmyspark1@gmail.com>
2023-09-20 19:17:15 -0700

    [feat] :- betty checks

--- core_funcs.c
+++ core_funcs.c
@@ -1,32 +1,32 @@
 #include "shell.h"
 
-#define TOKEN_DELIM " \t\r\n"
-
 /**
- * _strlen - a function that returns the length of a string
- * @str: character input str
- * Return: The length of str
+ * substitute_var - substitute variable if token is $
+ * @token: token
+ * Return: The token substituted
  */
 
-size_t _strlen(const char *str)
+char *substitute_var(char *token)
 {
-	size_t length = 0;
+	if (token[0] == '$')
+	{
+		char *value = getenv(token + 1);
 
-	while (*str++)
-		length++;
+		if (value)
+			return (value);
+	}
 
-	return (length);
+	return (token);
 }
-
 
 /**
  * read_line - read lines
  */
 
-char *read_line(void)
+char *read_line(char *name)
 {
 	char *line;
-	size_t buf = 0;
+	size_t buf = BUFFER_SIZE;
 
 	if (_getline(&line, &buf, stdin) == -1)
 	{
@@ -38,109 +38,82 @@ char *read_line(void)
 		else
 		{
 			free(line);
-			perror("./shell");
+			perror(name);
 			exit(EXIT_FAILURE);
 		}
 	}
 
 	return (line);
-
-}
-
-/**
- * split_line - split line into tokens
- * @line: line to be split
- *
- * Return: a pointer to the tokens
- */
-
-char **split_line(char *line)
-{
-	int i = 0, buf_size = BUFFER_SIZE;
-	char *split_str;
-	char **tokens;
-
-	tokens = malloc(sizeof(char *) * buf_size);
-
-	if (!tokens)
-	{
-		perror("./shell");
-		exit(EXIT_FAILURE);
-	}
-
-	split_str = _strtok(line, TOKEN_DELIM);
-
-	while (split_str != NULL)
-	{
-		tokens[i] = split_str;
-		i++;
-		split_str = _strtok(NULL, TOKEN_DELIM);
-
-	}
-
-	tokens[i] = NULL;
-	return (tokens);
-
 }
 
 /**
  * execute_args - execute the arguments using execve
  * @args: arguments to be executed
- *
+ * @name: name of the first argument
  */
 
-int execute_args(char **args)
+void execute_args(char **args, char *name)
 {
-	pid_t pid;
-	int status;
+	pid_t pid, wpid;
+	int i, status;
+	char *command_path;
 
-	char *command_path = find_command(args[0]);
 
+	for (i = 0; args[i]; i++)
+		args[i] = substitute_var(args[i]);
+	command_path = find_command(args[0]);
 	if (command_path == NULL)
 	{
-		return (-1);
+		fprintf(stderr, "%s: Command not found: %s\n", name, args[0]);
+		return;
 	}
-
 	pid = fork();
-
 	if (pid == 0)
 	{
 		if (execve(command_path, args, NULL) == -1)
 		{
-			perror("execve");
+			perror(name);
+			free(command_path);
 			exit(EXIT_FAILURE);
 		}
-		exit(EXIT_SUCCESS);
 	}
-	else if (pid > 0)
+	else if (pid < 0)
 	{
-		waitpid(pid, &status, 0);
-
-		if (WIFEXITED(status))
-		{
-			return (WEXITSTATUS(status));
-		}
-		else
-		{
-			fprintf(stderr,
-				"Child Process did not exit normally\n");
-			return (-1);
-		}
+		perror(name);
+		free(command_path);
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		perror("fork");
-		return (-1);
+		do
+			wpid = waitpid(pid, &status, WUNTRACED);
+		while
+			(!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
+	free(command_path);
 }
 
+/**
+ * find_command - find command
+ * @command: command to be found
+ */
 
 char *find_command(const char *command)
 {
-	char *path = getenv("PATH");
-	char *token, *saveptr, *full_path;
+	char *path_orig = getenv("PATH");
+	char *path = strdup(path_orig);
+	char *token, *full_path;
 
-	token = strtok_r(path, ":", &saveptr);
+	if (!path_orig)
+	{
+		free(path);
+		return (NULL);
+	}
+
+	if (!path)
+		return (NULL);
+
+	token = _strtok(path, ":");
 
 	while (token != NULL)
 	{
@@ -149,8 +122,8 @@ char *find_command(const char *command)
 		if (full_path == NULL)
 		{
 			perror("malloc");
-			free(full_path);
-			exit(EXIT_FAILURE);
+			free(path);
+			return (NULL);
 		}
 
 		strcpy(full_path, token);
@@ -158,10 +131,13 @@ char *find_command(const char *command)
 		strcat(full_path, command);
 
 		if (access(full_path, X_OK) == 0)
+		{
+			free(path);
 			return (full_path);
-
+		}
 		free(full_path);
-		token = strtok_r(NULL, ":", &saveptr);
+		token = _strtok(NULL, ":");
 	}
+	free(path);
 	return (NULL);
 }
