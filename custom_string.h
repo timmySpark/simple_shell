0a4824bb00023b1c09876b8baebd36bd47d2e7f5
Emmanuel Jerry <62827201+Emmzy17@users.noreply.github.com>
2023-09-23 04:49:57 +0100

    Update custom_string.h

--- custom_string.h
+++ custom_string.h
@@ -1,1 +1,9 @@
+/* ======================== STRING FUNCTIONS ======================== */
 
+int _strlen(char *c);
+int _strncmp(char *s1, char *s2, int n);
+char *_strchr(char *s, char c);
+char *_strdup(char *s);
+char *_strcat(char *dest, char *src);
+char *_strtok(char *str, char *delim);
+char *_strcpy(char *dest, char *src);
