23b1f04bcf85c07912f2d5af8744379f628e9ea3
Emmanuel Jerry <62827201+Emmzy17@users.noreply.github.com>
2023-09-23 04:08:39 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -125,6 +125,27 @@ char *userInput(cmd_t *commands, cmd_t *arguments);
 ssize_t _getline(char **buf_line, int *buf_size, int fd, buff_t *buff_info);
 ssize_t buffer_read(int fd, buff_t *buff_info);
 
+/* ====================== SPECIAL FUNCTIONS ======================== */
 
+int cases(cmd_t *cmmds, cmd_t *args, int cmd_count, int i, char *argv_0);
+int _setenv_cmd(cmd_t *args, cmd_t *cmmds, int cmd_count, int i, char *argv_0);
+int _unsetenv_cmd(cmd_t *args, cmd_t *cmmds, int cmd_count, int i, char *argv_0);
+int _exit_cmd(cmd_t *args, cmd_t *cmmds, int cmd_count, int i, char *argv_0);
+int _env_cmd(cmd_t *args, cmd_t *cmmds, int cmd_count, int i, char *argv_0);
+int cd_cmd(cmd_t *args, cmd_t *cmmds, int cmd_count, int i, char *argv_0);
+void cd(cmd_t *args, char *argv_0, int cmd_count);
+int show_home(cmd_t *args);
+int get_oldpwd(cmd_t *args);
+void clean(cmd_t *cmmds, cmd_t *args);
+char *check_hash(char *str);
+void cl_exec(cmd_t *cmmds, cmd_t *args, int cmd_count, char *arg);
+char *rmv_space(char *cmd);
+int exit_atoi(char *str);
+void _exits(cmd_t *cmmds, cmd_t *args, int cCt, int idx, char *arg);
+void looper(cmd_t *cmmds, cmd_t *args, char *argv_0, int *cmd_count);
+cmd_t *init_cmd_t();
+char *read_file(char *input_file, char *argv_0, cmd_t *cmmds, cmd_t *args);
+char *replace_char(char *str, char c, char s);
+char *rmv_double(char *cmd, char c);
 #endif
 
